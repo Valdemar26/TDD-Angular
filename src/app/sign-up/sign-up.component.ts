4361a646f303ba1163983d438477a21749490e50
volodymyrz <volodymyr@expandi.io>
2024-02-19 19:45:56 +0200

    feature:  - wrapped fields with form, implemented methods to submit form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -6,8 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  username = '';
+  email = '';
   password = '';
   repeatPassword = '';
+
+  onChangeUsername(event: Event): void {
+    this.username = (event.target as HTMLInputElement).value;
+  }
+
+  onChangeEmail(event: Event): void {
+    this.email = (event.target as HTMLInputElement).value;
+  }
 
   onChangePassword(event: Event): void {
     this.password = (event.target as HTMLInputElement).value;
@@ -15,5 +25,15 @@ export class SignUpComponent {
 
   onChangePasswordRepeat(event: Event): void {
     this.repeatPassword = (event.target as HTMLInputElement).value;
+  }
+
+  signUp(): void {
+    fetch("api/1.0/users", {
+      method: 'POST',
+      body: JSON.stringify({username: this.username, password: this.password, email: this.email}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
   }
 }
